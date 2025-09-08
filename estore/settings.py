614209bf16aeda93b91967c1c8495e57970b0bbd
Sokenu P. (Precious) <sokenu.precious2020@gmail.com>
2025-09-08 17:50:48 +0100

    first commit

--- estore/settings.py
+++ estore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z4+y_d6s*zpb-oyy8_3zjdmq5_k8naq)mody!7pw(q96r)xr8r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -150,11 +150,4 @@ DEFAULT_FROM_EMAIL = 'Sky Height Ventures <info.skyheightventures@gmail.com>'
 SITE_URL = "https://skyheightventures.com"
 
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = "finemdomains@gmail.com" # your email address
-# EMAIL_HOST_PASSWORD = "cctu hclb qgdr gzrp" # your password
-# DEFAULT_FROM_EMAIL = 'Shopwise <finemdomains@gmail.com>'
 
